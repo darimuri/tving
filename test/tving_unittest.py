8484e19789eb72635dff4b101e6ce73ba3574c1e
Goohyun Chung <goohyun.chung@linecorp.com>
2019-09-14 03:12:57 +0900

    remove test authentication information

--- test/tving_unittest.py
+++ test/tving_unittest.py
@@ -2,18 +2,19 @@
 import unittest   # The test framework
 import os
 import sys
+import auth
 from tving import api
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     api = None
     def setUp(self):
         self.api = api.TvingAPI("login.dat", "programlist.txt", "http://192.168.0.100:9999", "abcde")
-        isLogin, e = self.api.DoLogin("dormael", "dkanskrn12#", "CJONE")
+        isLogin, e = self.api.DoLogin(auth.id, auth.password, "CJONE")
         self.assertTrue(isLogin)
         self.assertEqual(e, "Log")
 
     def test_GetLoginData2(self):
-        loginData = self.api.GetLoginData2("dormael", "dkanskrn12#", "CJONE")
+        loginData = self.api.GetLoginData2(auth.id, auth.password, "CJONE")
         self.assertIsNotNone(loginData)
         self.assertIsNotNone(loginData['t'])
         self.assertTrue(loginData['t'].startswith("_tving_token="))
